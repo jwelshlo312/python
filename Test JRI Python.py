fdfc3c17003f039c37c78e1ee5fae7721d1d1106
jwelshlo312 <jwelsh-loveman@urban.org>
2017-02-17 14:08:45 -0500

    Add files via upload

---
+++ Test JRI Python.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 29 13:48:42 2016
+
+@author: jwelshlo
+"""
+
+#Change the path to your computer, make sure to use /
+path = 'D:/Users/JWelshlo/Documents/JRI'
+
+import pandas as pd
+
+# Change the working directory
+chdir()
+
+#Pandas has a read_csv function
+
+jri  = pd.read_csv ("JRI infographic data.csv")
